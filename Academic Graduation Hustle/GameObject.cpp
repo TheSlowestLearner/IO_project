a6a04fc176c200204b515bcb2381f54f40bb6226
TheSlowestLearner <macpop@student.agh.edu.pl>
2024-12-01 21:26:42 +0100

    Stworzono proste definicje bazowych funkcji.

--- Academic Graduation Hustle/GameObject.cpp
+++ Academic Graduation Hustle/GameObject.cpp
@@ -1,18 +1,21 @@
 #include "GameObject.h"
 
+constexpr char spriteFile[] = "";		// œcie¿ka do pliku z teksturami
+
 GameObject::GameObject()
+	: sprite(sf::Sprite()), texture(sf::Texture()), position(sf::Vector2f()), hidden(true)
 {
 
 }
 void GameObject::show()
 {
-
+	this->hidden = false;
 }
 void GameObject::hide()
 {
-
+	this->hidden = true;
 }
 bool GameObject::is_shown() const
 {
-	return false;
+	return !this->hidden;
 }
