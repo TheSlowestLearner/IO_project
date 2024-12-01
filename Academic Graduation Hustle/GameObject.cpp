29dac64d266de7b60b1151585578e3cb2bd4a02a
TheSlowestLearner <macpop@student.agh.edu.pl>
2024-12-01 21:15:43 +0100

    Dodano puste definicje funkcji

--- Academic Graduation Hustle/GameObject.cpp
+++ Academic Graduation Hustle/GameObject.cpp
@@ -1,1 +1,18 @@
 #include "GameObject.h"
+
+GameObject::GameObject()
+{
+
+}
+void GameObject::show()
+{
+
+}
+void GameObject::hide()
+{
+
+}
+bool GameObject::is_shown() const
+{
+	return false;
+}
