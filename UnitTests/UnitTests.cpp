506ba188f4d653549fd7eb064258d453c62dae1a
TheSlowestLearner <macpop@student.agh.edu.pl>
2024-12-17 15:34:23 +0100

    Przygotowano projekt pod unit testy

---
+++ UnitTests/UnitTests.cpp
@@ -0,0 +1,63 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Academic Graduation Hustle/main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(TaskLogic_Tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+	TEST_CLASS(Scene_Tests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+	TEST_CLASS(GameObjects_Tests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+	TEST_CLASS(ObjectCreators_Tests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+	TEST_CLASS(WindowHandler_Tests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+	TEST_CLASS(GameManager_Tests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
