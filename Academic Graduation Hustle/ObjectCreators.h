29c007dede7b834a0903b7803f168de36ced2e73
TheSlowestLearner <macpop@student.agh.edu.pl>
2025-01-01 18:51:59 +0100

    Wypełniono klasy nagłówka ObjectCreators.h

--- Academic Graduation Hustle/ObjectCreators.h
+++ Academic Graduation Hustle/ObjectCreators.h
@@ -1,11 +1,68 @@
 #pragma once
 #include"GameObjects.h"
 
+/// <summary>
+/// Abstrakcja klasy tworz¹cej renderowalne obiekty.
+/// </summary>
 class RenderableCreator
 {
 public:
 
 	virtual ~RenderableCreator() = 0;
+
+	static RenderableInstance* MakeRenderableInstance();
+
+private:
+
+
+};
+/// <summary>
+/// Klasa tworz¹ca instancje dekoracji.
+/// </summary>
+class DecorationCreator : RenderableCreator
+{
+public:
+
+	static Decoration* MakeDecoration();
+
+private:
+
+
+};
+/// <summary>
+/// Klasa tworz¹ca instancje pól tekstowych.
+/// </summary>
+class TextboxCreator : RenderableCreator
+{
+public:
+
+	static Textbox* MakeTextbox();
+
+private:
+
+
+};
+/// <summary>
+/// Klasa tworz¹ca instancje przycisków.
+/// </summary>
+class ButtonCreator : RenderableCreator
+{
+public:
+
+	static Button* MakeButton();
+
+private:
+
+
+};
+/// <summary>
+/// Klasa tworz¹ca instancje wyskakuj¹cych okienek.
+/// </summary>
+class PopupCreator : RenderableCreator
+{
+public:
+
+	static Popup* MakePopup();
 
 private:
 
