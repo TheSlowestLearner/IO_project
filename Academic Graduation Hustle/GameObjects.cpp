14098b1973e4e33b39b5e69632c8c45b9162ddfd
TheSlowestLearner <macpop@student.agh.edu.pl>
2025-01-12 14:40:14 +0100

    Dodanie przestrzeni nazw sp:: z przykładowymi pozycjami Sprite'ów w pliku.

--- Academic Graduation Hustle/GameObjects.cpp
+++ Academic Graduation Hustle/GameObjects.cpp
@@ -1,4 +1,13 @@
 #include "GameObjects.h"
 
-constexpr char spriteFile[] = "";		// œcie¿ka do pliku z teksturami
+constexpr char spriteFile[] = "graphics/spritesheet_placeholder.png";	// œcie¿ka do pliku z teksturami
+
+namespace sp
+{
+	const sf::Rect<int> ExampleSprite1(0, 0, 200, 200);
+	const sf::Rect<int> ExampleSprite2(200, 200, 200, 200);
+	const sf::Rect<int> ExampleSprite3(400, 400, 200, 200);
+	const sf::Rect<int> ExampleSprite4(600, 600, 200, 200);
+	const sf::Rect<int> ExampleSprite5(800, 800, 200, 200);
+}
 
